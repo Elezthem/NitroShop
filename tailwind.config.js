55b2a55099a290c54a2ec08c7d1acc1ac2400c9e
Elezthem <97851451+Elezthem@users.noreply.github.com>
2023-07-16 12:32:03 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.html'],
+  darkMode: `class`,
+  theme: {
+    extend: {
+      colors: {
+        mainColor: `#050C2A`
+      },
+      padding: {
+        big: `50px`
+      },
+      screens: {
+        sm: `480px`,
+        md: `768px`,
+        lg: `1028px`,
+        xl: `1440px`
+      }
+    },
+  },
+  plugins: [],
+}
+
